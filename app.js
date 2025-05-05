41aa4ec828c670bb70d1cd4d00d6c14227249c7b
ajikowiyu <ajikowiyu@gmail.com>
2025-05-05 20:10:07 +0700

    percobaan menggunakan express

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-let nama = "Ronaldo"
-console.log(`Selamat datang ${nama}`)
+const express   = require('express')
+const app       = express()
+
+
+app.get('/', (req, res)=>{
+  res.send('<h1>Hello World</h1><hr><hr><hr>')
+})
+
+
+app.listen(3000, ()=>{
+    console.log('Server sudah on, silakan akses http://localhost:3000')
+})
